0aff5fed4104d1628e8f004dbc4779fb2cc00bf4
Charles <charleshu32@gmail.com>
2019-07-23 03:21:03 -0400

    Put everything in a function and update as soon as app launch

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Store all of the name of dice in a array
+    let diceArray=["dice1","dice2","dice3","dice4","dice5",
+                   "dice6"];
+    
     //declear random variables
     var randomDiceIndex1:Int=0;
     var randomDiceIndex2:Int=0;
@@ -23,11 +27,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Change the dice images as soon as the dice app lunch
+        updateDiceImages();
     }
 
     @IBAction func rollButtonPress(_ sender: UIButton) {
+        updateDiceImages();
+    }
+    
+    func updateDiceImages(){
+        //Making two different random numbers for dice
         randomDiceIndex1=Int.random(in:0 ... 5);
         randomDiceIndex2=Int.random(in:0 ... 5);
+        
+        //Print for debug
+        print(randomDiceIndex1);
+        
+        //Change Dice image for the diceImageView1
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1]);
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2]);
     }
+    
 }
 
