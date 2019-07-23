6e4ade0ca2a4335a07dda22605533213ea8a4b25
Charles <charleshu32@gmail.com>
2019-07-23 02:48:06 -0400

    Do some random generate

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //declear random variables
+    var randomDiceIndex1:Int=0;
+    var randomDiceIndex2:Int=0;
+    
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPress(_ sender: UIButton) {
+        randomDiceIndex1=Int.random(in:0 ... 5);
+        randomDiceIndex2=Int.random(in:0 ... 5);
+    }
 }
 
